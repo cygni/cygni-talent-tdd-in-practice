37d61331cf79e19d7e920ae6328219962a28c99c
Pálmi Þór Valgeirsson <palmithor@Palmis-MacBook-Pro.local>
2019-11-12 09:04:44 +0100

    Implement tasks

--- src/test/java/se/cygni/palmithor/tdd/CalculatorTest.java
+++ src/test/java/se/cygni/palmithor/tdd/CalculatorTest.java
@@ -12,8 +12,18 @@ public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
-    public void test() {
-        assertThat(calculator.sumAll().get()).isEqualTo(1); // TODO failing on purpose please fix
+    public void testNull() {
+        assertThat(calculator.sumAll(null)).isEmpty();
+    }
+
+    @Test
+    public void testEmpty() {
+        assertThat(calculator.sumAll().get()).isEqualTo(0);
+    }
+
+    @Test
+    public void testValues() {
+        assertThat(calculator.sumAll(1,2).get()).isEqualTo(3);
     }
 
 
