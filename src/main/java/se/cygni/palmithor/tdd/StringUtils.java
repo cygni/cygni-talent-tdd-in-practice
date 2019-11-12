340fe842634bbeea0572a35b4fa8508f60a0fc6a
elisabeth <jensenelisabeth@gmail.com>
2019-11-12 15:46:46 +0100

    100%

--- src/main/java/se/cygni/palmithor/tdd/StringUtils.java
+++ src/main/java/se/cygni/palmithor/tdd/StringUtils.java
@@ -4,17 +4,32 @@ public class StringUtils {
 
 
     public boolean isPalindrome(final String str) {
-        throw new RuntimeException("Not yet implemented");
+        if (str == null) return false;
+
+        var maxIndex = str.length();
+        for (int i = 0; i < str.length(); i++) {
+            maxIndex--;
+            if (str.charAt(i) != str.charAt(maxIndex))
+                return false;
+        }
+        return true;
+
+        // (throw new RuntimeException("Not yet implemented");
     }
 
 
     /**
      * Checks if a String is empty (""), null or whitespace only.
-     * @param str the string to check
      *
+     * @param str the string to check
      * @return true if str is null, empty or whitespace only, otherwise false
      */
     public boolean isBlank(final String str) {
-        throw new RuntimeException("Not yet implemented");
+        if (str == null)
+            return true;
+        String s = str.strip();
+        if (s.isEmpty())
+            return true;
+        return false;
     }
 }
