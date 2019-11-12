7a4694ab6895039b79726b929e81b447b9cf55a8
Zhengyu Wang <zhengyu.wang@cygni.se>
2019-11-12 15:18:54 +0100

    ok tests

--- src/main/java/se/cygni/palmithor/tdd/StringUtils.java
+++ src/main/java/se/cygni/palmithor/tdd/StringUtils.java
@@ -4,7 +4,28 @@ public class StringUtils {
 
 
     public boolean isPalindrome(final String str) {
-        throw new RuntimeException("Not yet implemented");
+        if(str == null)
+            return false;
+        if(str.isEmpty() && str.isBlank())
+            return true;
+
+        String strCopy = new StringBuilder(str).reverse().toString();
+
+        if(strCopy.equalsIgnoreCase(str))
+            return true;
+        return false;
+/*
+
+
+        boolean result = false;
+
+        if (str == null) {
+            result = false;
+        }
+
+        if (str.equals("")) result = false;
+
+        return  result;*/
     }
 
 
@@ -15,6 +36,6 @@ public class StringUtils {
      * @return true if str is null, empty or whitespace only, otherwise false
      */
     public boolean isBlank(final String str) {
-        throw new RuntimeException("Not yet implemented");
+        return str == null || str.isEmpty() || str.equals("") || str.trim().isEmpty();
     }
 }
