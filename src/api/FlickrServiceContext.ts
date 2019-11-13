fa7a1b0f6c6e9bbe79080db355a58bdae82ebbac
Max Davidson <max.davidson@cygni.se>
2019-11-13 10:47:14 +0100

    Remove unused interface

--- src/api/FlickrServiceContext.ts
+++ src/api/FlickrServiceContext.ts
@@ -1,15 +1,10 @@
-import React, { createContext, useContext } from 'react';
+import { createContext, useContext } from 'react';
 import FlickrService from './FlickrService';
 import FlickrServiceMock from './FlickrServiceMock';
 
 const FlickrServiceContext = createContext<FlickrService>(
   new FlickrServiceMock(),
 );
-
-export interface FlickrServiceProviderProps {
-  flickrService: FlickrService;
-  children: React.ReactNode;
-}
 
 export function useFlickrService() {
   return useContext(FlickrServiceContext);
