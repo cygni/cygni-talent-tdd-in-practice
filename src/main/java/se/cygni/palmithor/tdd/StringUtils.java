813dfb68bd65d6ca50e4f71f59029ff5dbc6f147
Malin Hanak <malinehanak@gmail.com>
2019-11-13 15:22:16 +0100

    stringUtils test

--- src/main/java/se/cygni/palmithor/tdd/StringUtils.java
+++ src/main/java/se/cygni/palmithor/tdd/StringUtils.java
@@ -4,7 +4,22 @@ public class StringUtils {
 
 
     public boolean isPalindrome(final String str) {
-        throw new RuntimeException("Not yet implemented");
+        if(str == null) {
+            return false;
+        }
+
+        for(int i1 = 0; i1 != str.length() /2; i1++) {
+
+            int i2 = str.length() -1 - i1;
+            
+            if(str.charAt(i1) != str.charAt(i2)){
+                return false;
+            }
+        }
+
+        return true;
+
+        //throw new RuntimeException("Not yet implemented");
     }
 
 
@@ -15,6 +30,7 @@ public class StringUtils {
      * @return true if str is null, empty or whitespace only, otherwise false
      */
     public boolean isBlank(final String str) {
-        throw new RuntimeException("Not yet implemented");
+        return str == null || str == "" || str.trim().isEmpty();
+
     }
 }
