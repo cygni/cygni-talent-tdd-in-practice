9ee8ad644d0e1561c2202630cf6e21e47012a117
Filip Lindahl <filip.lindahl@fast2.se>
2019-11-13 17:06:12 +0100

    make a gallery appear.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,6 +5,7 @@ import { useRequest, useDebouncedValue } from '../utils';
 
 import classes from './App.module.css';
 import logo from './logo.svg';
+import { Photo } from '../api/FlickrService';
 
 export default function App() {
   const flickrService = useFlickrService();
@@ -19,21 +20,32 @@ export default function App() {
     ]),
   );
 
+  const onInput = (event: React.FormEvent<HTMLInputElement>) => {
+    setQuery(event.currentTarget.value);
+  };
+
+  /* function Photo(photo: Photo) {
+   *   return <img />;
+   * } */
+
+  const Photo = (photo: Photo) => (
+    <img
+      key={photo.id}
+      id={photo.id}
+      alt={photo.title}
+      src={photo.sizes[0].url}
+      width={photo.sizes[0].width}
+      height={photo.sizes[0].height}
+    />
+  );
+
   return (
-    <header className={classes.header}>
-      <img src={logo} className={classes.logo} alt="logo" />
-      <p>
-        Edit <code className={classes.code}>src/App.tsx</code> and save to
-        reload.
-      </p>
-      <a
-        className={classes.link}
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <div>
+      {loading ? <p>Loading</p> : <p> No photos found </p>}
+      {error ? <p role="alert">Oh no!</p> : null}
+      <label htmlFor="Search">Search</label>
+      <input id="Search" value={query} onChange={onInput}></input>
+      <div>{photos.map(Photo)}</div>
+    </div>
   );
 }
