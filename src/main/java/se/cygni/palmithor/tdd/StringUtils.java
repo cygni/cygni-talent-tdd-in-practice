58ccce6dcd2b13bf78e76d498cd78a471f958e28
Yousif Touma <yousif.touma@cygni.se>
2019-11-13 15:04:28 +0100

    Add java implementations and tests

--- src/main/java/se/cygni/palmithor/tdd/StringUtils.java
+++ src/main/java/se/cygni/palmithor/tdd/StringUtils.java
@@ -4,7 +4,21 @@ public class StringUtils {
 
 
     public boolean isPalindrome(final String str) {
-        throw new RuntimeException("Not yet implemented");
+        if (null == str) {
+            return false;
+        }
+        if (str.isBlank()) {
+            return true;
+        }
+        if (str.length() == 1) {
+            return true;
+        }
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
@@ -15,6 +29,9 @@ public class StringUtils {
      * @return true if str is null, empty or whitespace only, otherwise false
      */
     public boolean isBlank(final String str) {
-        throw new RuntimeException("Not yet implemented");
+        if (null == str || str.isBlank()) {
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/se/cygni/palmithor/tdd/CalculatorTest.java
+++ src/test/java/se/cygni/palmithor/tdd/CalculatorTest.java
@@ -12,8 +12,13 @@ public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
-    public void test() {
-        assertThat(calculator.sumAll().get()).isEqualTo(1); // TODO failing on purpose please fix
+    public void sumAll() {
+        assertThat(calculator.sumAll(null)).isEqualTo(Optional.empty());
+        assertThat(calculator.sumAll()).isEqualTo(Optional.of(0));
+        assertThat(calculator.sumAll(0, 1)).isEqualTo(Optional.of(1));
+        assertThat(calculator.sumAll(-1, 1, 2, 3)).isEqualTo(Optional.of(5));
+        assertThat(calculator.sumAll(null, 1, 2, null, 3)).isEqualTo(Optional.of(6));
+        assertThat(calculator.sumAll(Integer.MAX_VALUE, 1)).isEqualTo(Optional.of(Integer.MIN_VALUE));
     }
 
 
