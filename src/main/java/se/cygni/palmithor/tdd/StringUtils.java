b9cb34ca9413bf7b46b0f18a65746868412956e2
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2019-11-13 15:09:36 +0100

    Fix 100% coverage

--- src/main/java/se/cygni/palmithor/tdd/StringUtils.java
+++ src/main/java/se/cygni/palmithor/tdd/StringUtils.java
@@ -7,12 +7,7 @@ public class StringUtils {
         if(str == null) {
             return false;
         }
-        for(int i = 0; i < str.length() / 2; ++i) {
-            if(str.charAt(i) != str.charAt(str.length() - 1 - i)) {
-                return false;
-            }
-        }
-        return true;
+        return new StringBuilder(str).reverse().toString().equals(str);
     }
 
 
