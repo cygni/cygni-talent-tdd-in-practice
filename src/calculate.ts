05d0ebe4018dffad3290c1be3fb13f024b395553
norenma <markus.cygni@outlook.com>
2021-09-22 12:55:53 +0200

    The solution

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,10 +1,44 @@
-export function calculate(input: string): string {
-  if (input.includes("+")) {
-    const [first, second] = input.split("+");
-    return (Number(first) + Number(second)).toString();
-  } else if (input.includes("-")) {
-    const [first, second] = input.split("-");
-    return (Number(first) - Number(second)).toString();
+const binaryOperate = (
+  a: number,
+  b: number,
+  operator: "+" | "-" | "*" | "/"
+) => {
+  if (operator === "+") {
+    return a + b;
+  } else if (operator === "-") {
+    return a - b;
+  } else if (operator === "*") {
+    return a * b;
+  } else {
+    return a / b;
   }
-  return "";
+};
+
+const subRegex = /(?<!\*|\/)-/;
+
+const reduceOperator = (numbers: string[], operator: "+" | "-" | "*" | "/") => {
+  return numbers.slice(1).reduce((sum: number, curr: string) => {
+    return binaryOperate(sum, _calculate(curr), operator);
+  }, _calculate(numbers[0]));
+};
+
+export const calculate = (input: string): string => {
+  return _calculate(input).toString();
+};
+
+function _calculate(input: string): number {
+  if (input.includes("+")) {
+    const numbers = input.split("+");
+    return reduceOperator(numbers, "+");
+  } else if (input.split(subRegex).length > 1) {
+    const numbers = input.split(subRegex);
+    return reduceOperator(numbers, "-");
+  } else if (input.includes("*")) {
+    const numbers = input.split("*");
+    return reduceOperator(numbers, "*");
+  } else if (input.includes("/")) {
+    const numbers = input.split("/");
+    return reduceOperator(numbers, "/");
+  }
+  return Number(input);
 }
