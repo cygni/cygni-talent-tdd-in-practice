83be9d69fc7d3b2f74ecab940f2132c1437315b8
norenma <markus.cygni@outlook.com>
2021-09-22 12:55:53 +0200

    The solution

--- src/calculate.test.ts
+++ src/calculate.test.ts
@@ -6,14 +6,42 @@ it("should add two numbers", () => {
 it("should subtract two numbers", () => {
   expect(calculate("10-5")).toEqual("5");
   expect(calculate("5-5")).toEqual("0");
+  expect(calculate("-5")).toEqual("-5");
 });
 
 it("should multiply two numbers", () => {
-  throw Error("Not implemented.");
+  expect(calculate("-3*5")).toEqual("-15");
+  expect(calculate("3*-5")).toEqual("-15");
 });
 
-it("should divide two numbers", () => {
-  throw Error("Not implemented.");
+it("should divivde two numbers", () => {
+  expect(calculate("15/5")).toEqual("3");
+  expect(calculate("15/-5")).toEqual("-3");
 });
 
-// Write more test cases as you go.
+it("should add more than two numbers", () => {
+  expect(calculate("5+5+3")).toEqual("13");
+});
+
+it("should multiply more than two numbers", () => {
+  expect(calculate("2*3*4")).toEqual("24");
+});
+
+it("should divivde more than two numbers", () => {
+  expect(calculate("16/2/2")).toEqual("4");
+});
+
+it("should caluclate a mix of multiplication and divison", () => {
+  expect(calculate("2*4/2")).toEqual("4");
+  expect(calculate("5*14/16*8/10")).toEqual("3.5");
+});
+
+it("should caluclate a mix of multiplication and subtraction", () => {
+  expect(calculate("-10*2")).toEqual("-20");
+});
+
+it("should caluclate a mix of multiplication, divison and subtraction", () => {
+  expect(calculate("10-5*3")).toEqual("-5");
+  expect(calculate("-10*15")).toEqual("-150");
+  expect(calculate("16*7/-14+15/10*-2")).toEqual("-11");
+});
